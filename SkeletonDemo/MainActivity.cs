f363cae9dffb5dc8b6568037c63c1eeff1416b46
hinal.patel <hinal.patel@1R-79>
2022-03-10 16:38:53 +0530

    Add project files.

---
+++ SkeletonDemo/MainActivity.cs
@@ -0,0 +1,101 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using AndroidX.RecyclerView.Widget;
+using System.Collections.Generic;
+using System.Threading;
+using Supercharge;
+using Android.Widget;
+using Java.Lang;
+using System;
+using System.Threading.Tasks;
+
+namespace SkeletonDemo
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TextView tv;
+        ShimmerLayout shimmerLayout;
+        List<string> employeenames;
+        LinearLayout linearLayout;
+
+
+        RecyclerView.LayoutManager mLayoutManager;
+        RecyclerView recycler;
+        adepter mAdapter;
+        int c = 0;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            shimmerLayout = FindViewById<ShimmerLayout>(Resource.Id.shimeerlayout);
+            linearLayout = FindViewById<LinearLayout>(Resource.Id.linearLayout1);
+            tv = FindViewById<TextView>(Resource.Id.textView);
+            recycler = FindViewById<RecyclerView>(Resource.Id.recyclerView);
+            recycler.Visibility = Android.Views.ViewStates.Invisible;
+            employeenames = new List<string> {  };
+
+            employeenames = new List<string> {
+
+            "Akk",
+            "Rosk",
+            "Rock",
+            "Pinky",
+            "Redy",
+            "Whit",
+            "Yello",
+            "Black",
+            "Akk",
+            "Rosk",
+            "Rock",
+            "Pinky",
+            "Redy",
+            "Whit",
+            "Yello",
+            "Black",
+            "Akk",
+            "Rosk",
+            "Rock",
+            "Pinky",
+            "Redy",
+            "Whit",
+            "Yello",
+            "Black",
+            "Akk",
+            "Rosk",
+            "Rock",
+            "Pinky",
+            "Redy",
+            "Whit",
+            "Yello",
+            "Black",
+            };
+
+
+            mLayoutManager = new LinearLayoutManager(this);
+            recycler.SetLayoutManager(mLayoutManager);
+            mAdapter = new adepter(employeenames, ApplicationContext);
+            recycler.SetAdapter(mAdapter);
+            shimmerLayout.StartShimmerAnimation();
+
+
+
+        }
+        protected override async void OnResume()
+        {
+            base.OnResume();
+            await Task.Delay(3000);
+            recycler.Visibility = Android.Views.ViewStates.Visible;
+            shimmerLayout.StopShimmerAnimation();
+            linearLayout.Visibility = Android.Views.ViewStates.Invisible;
+
+        }
+
+
+
+    }
+}
---
+++ SkeletonDemo/adepter.cs
@@ -0,0 +1,73 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.RecyclerView.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkeletonDemo
+{
+    class adepter : RecyclerView.Adapter
+    {
+        Context con;
+        private readonly string[] spaceCrafts;
+        public event EventHandler<int> itemClick;
+        public List<string> employeenames;
+
+        public adepter(List<string> employeenames, Context context)
+        {
+            this.employeenames = employeenames;
+            con = context;
+        }
+
+        //public override int ItemCount => throw new NotImplementedException();
+        public override int ItemCount
+        {
+            get { return employeenames.Count; }
+        }
+
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            MyViewHolder vh = holder as MyViewHolder;
+
+            vh.textView.Text = employeenames[position].ToString();
+            vh.textView.Click += delegate {
+                Toast.MakeText(con, "message", ToastLength.Long).Show();
+            };
+        }
+
+
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View itemView = LayoutInflater.From(parent.Context).
+                Inflate(Resource.Layout.item, parent, false);
+            MyViewHolder vh = new MyViewHolder(itemView);
+            return vh;
+        }
+
+
+
+        public class MyViewHolder : RecyclerView.ViewHolder
+        {
+            private readonly Action<int> listener;
+            public TextView textView;
+            public MyViewHolder(View itemView) : base(itemView)
+            {
+
+                textView = itemView.FindViewById<TextView>(Resource.Id.textitem);
+
+
+            }
+
+
+        }
+
+    }
+}
